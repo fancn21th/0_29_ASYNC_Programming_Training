5ecab59dc5305f55c58b0836059a1cfe4cd811ce
alex.fan <alex.fan@okchem.com>
2017-11-12 10:12:39 +0800

    async demo update    Concurrently

--- async_await/sixth_async.js
+++ async_await/sixth_async.js
@@ -5,11 +5,13 @@ const fetch = require('node-fetch')
 async function fetchRandomNumberAndRandomText() {
   const numberPromise = fetch(url)
   const textPromise = fetch(url2)
+
   const number = await numberPromise
-  const text = await textPromise
   const numberVal = await number.json()
-  const textVal = await text.json()
   console.log(numberVal.data.foo)
+
+  const text = await textPromise
+  const textVal = await text.json()
   console.log(textVal.data.foo)
 }
 
